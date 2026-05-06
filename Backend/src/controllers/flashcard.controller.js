4544e7d84c163a384498703219f26ce7449c5a25
prince728 <78577947+prince728@users.noreply.github.com>
2026-05-06 22:11:29 +0530

    create delete ans save route for flashcard

---
+++ Backend/src/controllers/flashcard.controller.js
@@ -0,0 +1,81 @@
+const flashCardModel = require('../models/flashcard.model');
+const express = require('express');
+
+const generateFlashCard = async (req, res) => {
+    res.send('generate')
+
+}
+
+
+const deleteFlashCard = async (req, res) => {
+
+    try {
+        const flashcardId = req.params.id;
+
+        const flashcard = await flashCardModel.findOneAndDelete({
+            _id: flashcardId,
+            userId: req.user
+        });
+
+        if (!flashcard) {
+            return res.status(404).json({
+                message: "flashcard not found"
+            })
+        }
+
+        return res.status(200).json({
+            message: "flashcard deleted successfully",
+            id: flashcardId
+        })
+    } catch (error) {
+        console.error("Internal server error:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+
+const saveFlashCard = async (req, res) => {
+    try {
+        const { question, answer, subject } = req.body;
+
+        if (!question || !answer) {
+            return res.status(400).json({
+                message: "Please provide require field"
+            })
+        }
+
+        const flashcard = await flashCardModel.create({
+            question,
+            answer,
+            subject,
+            userId:req.user
+        });
+
+        return res.status(201).json({
+            message:"flashcard save successfully",
+            flashcard:{
+                question:flashcard.question,
+                answer:flashcard.answer,
+                subject:flashcard.subject
+            }
+        })
+
+    } catch (error) {
+        console.error("Internal server error:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+
+}
+
+
+const getAllFlashCard = async (req, res) => {
+    res.send('get all')
+}
+
+
+module.exports = {
+    saveFlashCard,
+    getAllFlashCard,
+    deleteFlashCard,
+    generateFlashCard
+};
--- Backend/src/routes/flashcard.routes.js
+++ Backend/src/routes/flashcard.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const flashCardController =  require('../controllers/flashcard.controller')
+const flashCardController = require('../controllers/flashcard.controller');
+const { authUserMiddleware } = require('../middlewares/auth.middleware')
 
 const flashCardRouter = express.Router();
 
-flashCardRouter.post('/generate',flashCardController.generateFlashCard )
-flashCardRouter.post('/', flashCardController.saveFlashCard)
-flashCardRouter.get('/',flashCardController.getAllFlashCard )
-flashCardRouter.delete('/:id', flashCardController.deleteFlashCard )
+flashCardRouter.post('/generate', authUserMiddleware, flashCardController.generateFlashCard)
+flashCardRouter.post('/', authUserMiddleware, flashCardController.saveFlashCard)
+flashCardRouter.get('/', authUserMiddleware, flashCardController.getAllFlashCard)
+flashCardRouter.delete('/:id', authUserMiddleware, flashCardController.deleteFlashCard)
 
 
 module.exports = flashCardRouter;
