725b13ec600c5b325f446acc23dc1515416a6250
prince728 <78577947+prince728@users.noreply.github.com>
2026-05-06 21:02:07 +0530

    create all required flashcard routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express =  require('express');
 const AuthRouter = require('./routes/user.routes');
 const cookieParser = require('cookie-parser');
+const flashCardRouter = require('./routes/flashcard.routes');
 
 const app =  express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use('/api/auth', AuthRouter);
+app.use('/api/flashcard', flashCardRouter);
 
 
 module.exports = app;
---
+++ Backend/src/routes/flashcard.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const flashCardController = require('../controllers/flashcard.controller');
+
+const flashCardRouter = express.Router();
+
+flashCardRouter.post('/generate',flashCardController.generateFlashCard )
+flashCardRouter.post('/', flashCardController.saveFlashCard)
+flashCardRouter.get('/',flashCardController.getAllFlashCard )
+flashCardRouter.delete('/:id', flashCardController.deleteFlashCard )
+
+
+module.exports = flashCardRouter;
